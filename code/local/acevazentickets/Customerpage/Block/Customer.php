5585cef572255545349da8d11e05918b3bb7bd97
nkulavic <nick@Nicks-Mac-mini.local>
2015-04-21 15:50:03 -0500

    Removal Branding

--- code/local/acevazentickets/Customerpage/Block/Customer.php
+++ code/local/acevazentickets/Customerpage/Block/Customer.php
@@ -1,9 +1,5 @@
 <?php
-/**
- * customize by: http://acevazenticketsphp.com
- * like us on facebook: http://www.facebook.com/pages/acevazenticketsphp/235451993242578
- * follow us on twitter: http://twitter.com/acevazenticketsphp
- */
+
 class acevazenticketsphp_Customerpage_Block_Customer extends Mage_Customer_Block_Account_Dashboard  {
 
 }
--- code/local/acevazentickets/Customerpage/controllers/AccountpageController.php
+++ code/local/acevazentickets/Customerpage/controllers/AccountpageController.php
@@ -1,9 +1,5 @@
 <?php 
-/**
- * customize by: http://acevazenticketsphp.com
- * like us on facebook: http://www.facebook.com/pages/acevazenticketsphp/235451993242578
- * follow us on twitter: http://twitter.com/acevazenticketsphp
- */
+
 class acevazenticketsphp_Customerpage_AccountpageController extends Mage_Core_Controller_Front_Action {
 
     protected function _getSession() {
@@ -16,8 +12,6 @@ class acevazenticketsphp_Customerpage_AccountpageController extends Mage_Core_Co
                 $this->setFlag('', 'no-dispatch', true);
         }
     }
-
-    // http://localhost/acevazenticketsphp_magento/custompage/accountpage/index
     public function indexAction() { // landing page
         $this->loadLayout();
         $this->renderLayout();

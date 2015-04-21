9e2dca6180b0e12015be536fa1b8183ba7fbfb06
nkulavic <nick@Nicks-Mac-mini.local>
2015-04-21 15:48:04 -0500

    Updates Cont.

--- code/local/acevazentickets/Customerpage/Block/Customer.php
+++ code/local/acevazentickets/Customerpage/Block/Customer.php
@@ -1,1 +1,11 @@
-<?php/** * customize by: http://istockphp.com * like us on facebook: http://www.facebook.com/pages/Istockphp/235451993242578 * follow us on twitter: http://twitter.com/istockphp */class Istockphp_Customerpage_Block_Customer extends Mage_Customer_Block_Account_Dashboard  {}
+<?php
+/**
+ * customize by: http://acevazenticketsphp.com
+ * like us on facebook: http://www.facebook.com/pages/acevazenticketsphp/235451993242578
+ * follow us on twitter: http://twitter.com/acevazenticketsphp
+ */
+class acevazenticketsphp_Customerpage_Block_Customer extends Mage_Customer_Block_Account_Dashboard  {
+
+}
+
+
